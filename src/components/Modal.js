ff3576bf672f7bc6063fa7583aac3b27aefa880e
Ian <38500123+IBanda@users.noreply.github.com>
2021-02-09 21:54:55 +0800

    add close button to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -10,10 +10,26 @@ import { observer } from 'mobx-react-lite';
 const StyledDiv = styled.div`
   padding: 1em;
 
-  img.cover {
-    max-width: 300px;
-    margin-bottom: 1em;
-    width: 100%;
+  .pod-data {
+    img.cover {
+      max-width: 300px;
+      margin-bottom: 1em;
+      width: 100%;
+    }
+
+    .pod-desc-simple-bar {
+      max-height: 10em;
+    }
+    @media (max-width: 576px) {
+      img.cover {
+        max-width: 150px;
+        margin: 0 auto 1em;
+      }
+      .pod-desc-simple-bar {
+        max-height: 7em;
+        margin-bottom: 16px;
+      }
+    }
   }
   .title {
     font-weight: 600;
@@ -51,19 +67,20 @@ export default observer(function AppModal({ podcastId, show, onHide }) {
   };
   return (
     <Modal centered show={show} onHide={onHide} size="lg">
+      <Modal.Header className="px-3 py-1 border-0" closeButton />
       <StyledDiv className="modal-cstbody">
         {!(status === 'pending') ? (
           <div className="row">
-            <div className="col-lg-5">
+            <div className="col-lg-5 pod-data">
               <img className="cover" src={data?.image} alt="cover" />
               <h6 className="title">{data.title}</h6>
-              <SimpleBar style={{ maxHeight: '10em' }}>
+              <SimpleBar className="pod-desc-simple-bar">
                 <div className="desc-container">
                   {compiler(data?.description || '')}
                 </div>
               </SimpleBar>
             </div>
-            <div className="col-lg-7">
+            <div className="col-lg-7 ">
               <SimpleBar style={{ maxHeight: '32em' }}>
                 <ul>
                   {data?.episodes?.map((episode) => (
